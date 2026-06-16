d61153480907dc5a0be0904d356a46d83a47170a
cokokompony-arch <cokokompony@gmail.com>
2026-06-16 10:27:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,27 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
 
-keyboard = [
-    [InlineKeyboardButton("➕ Host Bot", callback_data="host")],
-    [InlineKeyboardButton("📂 My Bots", callback_data="mybots")],
-    [InlineKeyboardButton("💎 Premium", callback_data="premium")],
-    [InlineKeyboardButton("👤 Account", callback_data="account")],
-    [InlineKeyboardButton("ℹ️ Help", callback_data="help")]
-]
+TOKEN = os.getenv("TOKEN")
 
-reply_markup = InlineKeyboardMarkup(keyboard)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-await update.message.reply_text(
-    "👋 Welcome Michael 🚀",
-    reply_markup=reply_markup
-)
+    keyboard = [
+        [InlineKeyboardButton("➕ Host Bot", callback_data="host")],
+        [InlineKeyboardButton("📂 My Bots", callback_data="mybots")],
+        [InlineKeyboardButton("💎 Premium", callback_data="premium")],
+        [InlineKeyboardButton("👤 Account", callback_data="account")],
+        [InlineKeyboardButton("ℹ️ Help", callback_data="help")]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "👋 Welcome Michael 🚀",
+        reply_markup=reply_markup
+    )
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
