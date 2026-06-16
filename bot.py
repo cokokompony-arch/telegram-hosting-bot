12636b57735fbcdd3e92aaf45f9632e93ce26fb9
cokokompony-arch <cokokompony@gmail.com>
2026-06-16 09:18:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 TOKEN = os.getenv("8618104541:AAEigggCTvKYT46unTjO5LQoFRZ4xDZWm2g")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🚀 Hosting Bot Online!")
+    await update.message.reply_text("🚀 Hosting Bot Running!")
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
