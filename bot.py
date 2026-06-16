282325274eea4783070b6806fa1d9ded0188c752
cokokompony-arch <cokokompony@gmail.com>
2026-06-16 09:05:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("8618104541:AAEigggCTvKYT46unTjO5LQoFRZ4xDZWm2g")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🚀 Hosting Bot Online!")
